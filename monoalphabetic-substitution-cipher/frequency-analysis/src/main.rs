0e516120afb31be717a527b400df8506bf910544
C <DeveloperC@protonmail.com>
2019-10-04 19:55:35 +0000

    Updated frequency-analysis borrowing usage

--- monoalphabetic-substitution-cipher/frequency-analysis/src/main.rs
+++ monoalphabetic-substitution-cipher/frequency-analysis/src/main.rs
@@ -1,5 +1,3 @@
-#[macro_use] extern crate structopt;
-
 use structopt::StructOpt;
 use std::fs;
 use std::collections::HashMap;
@@ -20,13 +18,13 @@ fn main() {
 
     let ciphertext:String= read_file(args.input);
  
-    let mut letter_frequency:HashMap<char, u32> = calculate_letter_frequency(ciphertext.clone());
+    let mut letter_frequency:HashMap<char, u32> = calculate_letter_frequency(&ciphertext);
     let mut plaintext_chars:Vec<char> = ciphertext.clone().chars().collect();
 
     for i in 0..26 {  
-        let cipher_character:char = get_next_most_frequent(letter_frequency.clone());
+        let cipher_character:char = get_next_most_frequent(&letter_frequency);
     	letter_frequency.remove(&cipher_character);
-        plaintext_chars = replace_all_occurances(ciphertext.clone(), plaintext_chars, cipher_character, EXPECTED_LETTER_FREQUENCY[i]); 
+        plaintext_chars = replace_all_occurances(&ciphertext, plaintext_chars, cipher_character, EXPECTED_LETTER_FREQUENCY[i]); 
     }
 
     let plaintext:String = plaintext_chars.iter().collect();
@@ -40,7 +38,7 @@ fn read_file(filename:String) -> String {
     return file_contents.to_ascii_lowercase();
 }
 
-fn calculate_letter_frequency(ciphertext:String) -> HashMap<char, u32> {
+fn calculate_letter_frequency(ciphertext:&str) -> HashMap<char, u32> {
     let mut letter_frequency:HashMap<char, u32> = HashMap::new();
 
     for (_index, character) in ciphertext.chars().enumerate() {
@@ -53,7 +51,7 @@ fn calculate_letter_frequency(ciphertext:String) -> HashMap<char, u32> {
     return letter_frequency;
 }
 
-fn get_next_most_frequent(letter_frequency:HashMap<char, u32>) -> char {
+fn get_next_most_frequent(letter_frequency:&HashMap<char, u32>) -> char {
     let mut cipher_character:char = *(letter_frequency.keys().next()).unwrap();
     let mut largest_value = *(letter_frequency.get(&cipher_character)).unwrap();
 
@@ -67,7 +65,7 @@ fn get_next_most_frequent(letter_frequency:HashMap<char, u32>) -> char {
     return cipher_character;
 }
 
-fn replace_all_occurances(orginal_copy:String, mut modifying_copy:Vec<char>, replacing:char, replace_with:char) -> Vec<char> { 
+fn replace_all_occurances(orginal_copy:&str, mut modifying_copy:Vec<char>, replacing:char, replace_with:char) -> Vec<char> { 
     for (index, character) in orginal_copy.chars().enumerate() {
         if character == replacing {
             modifying_copy[index] = replace_with; 
