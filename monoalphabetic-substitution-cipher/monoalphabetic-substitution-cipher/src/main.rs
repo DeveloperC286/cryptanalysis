315016e355d13a6e8c7e93e148c7c6fe858a1ffb
C <DeveloperC@protonmail.com>
2019-10-05 17:20:04 +0000

    Refactored encipher big O from O(n^2) to O(n)

--- monoalphabetic-substitution-cipher/monoalphabetic-substitution-cipher/src/main.rs
+++ monoalphabetic-substitution-cipher/monoalphabetic-substitution-cipher/src/main.rs
@@ -85,13 +85,12 @@ fn invert_key(key:Vec<char>) -> Vec<char> {
 fn encipher(key:Vec<char>, file_contents:String) -> String { 
     let mut subsituting_file_contents: Vec<char> = file_contents.chars().collect();
 
-    for x in 0..26 {  
-        for (index, character) in file_contents.chars().enumerate() { 
-            if character == ASCII[x] {
-                subsituting_file_contents[index] = key[x];  
-            }
-        } 
-    } 
+    for (index, character) in file_contents.chars().enumerate() {
+        if ALPHABET.contains_key(&character) {
+            let i = ALPHABET.get(&character).unwrap()-1;
+            subsituting_file_contents[index] = key[i as usize];
+        }
+    }  
 
     return subsituting_file_contents.iter().collect();
 }
@@ -108,7 +107,7 @@ fn validate_key(key:String) -> Vec<char> {
 
     if key.len() != 26 {
         println!("The key must be of length 26.");
-         exit(1);
+        exit(1);
     }
 
     return key.chars().collect();
@@ -119,7 +118,6 @@ mod tests {
     use super::*;
     use rstest::rstest_parametrize;
 
-
     #[rstest_parametrize(expected, key,
         case("otduxbylwjmqifarkzphencvgs", "ofwcunytmjqhkvaslpzbdxiegr"),
         case("pqgitynxwrovjlkmfsduhzebca", "zxyswqcudmonpgkabjretlihfv"),
@@ -128,9 +126,22 @@ mod tests {
     fn test_invert_key(expected:&str, key:&str) {  
         //when
         let inverted_key = invert_key(key.chars().collect());
-        let expected_vector:Vec<char>= expected.chars().collect();
 
         //then
+        let expected_vector:Vec<char>= expected.chars().collect();
         assert_eq!(expected_vector, inverted_key);
+    }
+ 
+    #[rstest_parametrize(key, enciphering, expected, 
+        case("gqcerpnjmhtivalosbzwxdkfyu", "defend the east wall of the castle", "erprae wjr rgzw kgii lp wjr cgzwir"),
+        case("ouknpajzbhlvdrqcsxftiemwgy", "the simple substitution cipher is a cipher that has been in use for many hundreds of years", "tzp fbdcvp fiuftbtitbqr kbczpx bf o kbczpx tzot zof uppr br ifp aqx dorg zirnxpnf qa gpoxf"),
+        case("ypzvihmcgqobntajdwkeruxfls", "the simple substitution cipher is quite easy to break", "eci kgnjbi krpkegeregat zgjciw gk drgei iykl ea pwiyo"),
+    )]
+    fn test_encipher(key:&str, enciphering:&str, expected:&str) {  
+        //when
+        let returned = encipher(key.chars().collect(), enciphering.to_string()); 
+
+        //then
+        assert_eq!(expected, returned);
     }
 }
