72257ac624430543577fcd1ccc5fab1a212718e8
C <DeveloperC@protonmail.com>
2019-10-05 16:00:58 +0000

    Reduced inverting_key big O from O(n^2) to O(n)

--- monoalphabetic-substitution-cipher/monoalphabetic-substitution-cipher/src/main.rs
+++ monoalphabetic-substitution-cipher/monoalphabetic-substitution-cipher/src/main.rs
@@ -1,6 +1,43 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::process::exit;
 use std::fs;
 use structopt::StructOpt;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref ALPHABET: HashMap<char, u32> = {
+        let mut m = HashMap::new();
+        m.insert('a', 1);
+        m.insert('b', 2);
+        m.insert('c', 3);
+        m.insert('d', 4);
+        m.insert('e', 5);
+        m.insert('f', 6);
+        m.insert('g', 7);
+        m.insert('h', 8);
+        m.insert('i', 9);
+        m.insert('j', 10);
+        m.insert('k', 11);
+        m.insert('l', 12);
+        m.insert('m', 13);
+        m.insert('n', 14);
+        m.insert('o', 15);
+        m.insert('p', 16);
+        m.insert('q', 17);
+        m.insert('r', 18);
+        m.insert('s', 19);
+        m.insert('t', 20);
+        m.insert('u', 21);
+        m.insert('v', 22);
+        m.insert('w', 23);
+        m.insert('x', 24);
+        m.insert('y', 25);
+        m.insert('z', 26);
+        m
+    };
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "monoalphabetic-substitution-cipher", about = "A rust implementation of a monoalphabetic substitution cipher.")]
@@ -37,12 +74,9 @@ fn main() {
 fn invert_key(key:Vec<char>) -> Vec<char> {
     let mut inverting_key = ASCII.clone().to_vec();
 
-    for x in 0..26 {
-        for i in 0..26 {
-            if key[x] == ASCII[i] {
-                inverting_key[i] = ASCII[x]
-            }
-        }
+    for x in 0..key.len() {
+        let i = ALPHABET.get(&key[x]).unwrap()-1;
+        inverting_key[i as usize] = ASCII[x];
     }
 
     return inverting_key;
