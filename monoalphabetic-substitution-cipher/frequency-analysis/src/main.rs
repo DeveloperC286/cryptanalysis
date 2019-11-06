4519f53fe814b6fb0880188bd5bf770a5e1bd740
C <DeveloperC@protonmail.com>
2019-11-06 23:16:48 +0000

    Formatted rust code

--- monoalphabetic-substitution-cipher/frequency-analysis/src/main.rs
+++ monoalphabetic-substitution-cipher/frequency-analysis/src/main.rs
@@ -1,45 +1,61 @@
-use structopt::StructOpt;
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "frequency-analysis", about = "A rust implementation of a frequency analysis technique upon monoalphabetic substitution ciphers.")]
+#[structopt(
+    name = "frequency-analysis",
+    about = "A rust implementation of a frequency analysis technique upon monoalphabetic substitution ciphers."
+)]
 struct Args {
-    #[structopt(long = "input", help = "The path to a file containing the ciphertext to perform frequency analysis upon.")]
+    #[structopt(
+        long = "input",
+        help = "The path to a file containing the ciphertext to perform frequency analysis upon."
+    )]
     input: String,
 }
 
-static ALPHABET: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+static ALPHABET: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
 
-static EXPECTED_LETTER_FREQUENCY: [char; 26] = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'u', 'c', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'];
+static EXPECTED_LETTER_FREQUENCY: [char; 26] = [
+    'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'u', 'c', 'm', 'w', 'f', 'g', 'y', 'p',
+    'b', 'v', 'k', 'j', 'x', 'q', 'z',
+];
 
 fn main() {
-    let args = Args::from_args(); 
+    let args = Args::from_args();
 
-    let ciphertext:String= read_file(args.input);
- 
-    let mut letter_frequency:HashMap<char, u32> = calculate_letter_frequency(&ciphertext);
-    let mut plaintext_chars:Vec<char> = ciphertext.clone().chars().collect();
+    let ciphertext: String = read_file(args.input);
 
-    for i in 0..26 {  
-        let cipher_character:char = get_next_most_frequent(&letter_frequency);
-    	letter_frequency.remove(&cipher_character);
-        plaintext_chars = replace_all_occurances(&ciphertext, plaintext_chars, cipher_character, EXPECTED_LETTER_FREQUENCY[i]); 
+    let mut letter_frequency: HashMap<char, u32> = calculate_letter_frequency(&ciphertext);
+    let mut plaintext_chars: Vec<char> = ciphertext.clone().chars().collect();
+
+    for i in 0..26 {
+        let cipher_character: char = get_next_most_frequent(&letter_frequency);
+        letter_frequency.remove(&cipher_character);
+        plaintext_chars = replace_all_occurances(
+            &ciphertext,
+            plaintext_chars,
+            cipher_character,
+            EXPECTED_LETTER_FREQUENCY[i],
+        );
     }
 
-    let plaintext:String = plaintext_chars.iter().collect();
+    let plaintext: String = plaintext_chars.iter().collect();
     print!("{}", plaintext);
 }
 
-fn read_file(filename:String) -> String {
-    let file_contents = fs::read_to_string(filename)
-        .expect("Unable to read file.");
+fn read_file(filename: String) -> String {
+    let file_contents = fs::read_to_string(filename).expect("Unable to read file.");
 
     return file_contents.to_ascii_lowercase();
 }
 
-fn calculate_letter_frequency(ciphertext:&str) -> HashMap<char, u32> {
-    let mut letter_frequency:HashMap<char, u32> = HashMap::new();
+fn calculate_letter_frequency(ciphertext: &str) -> HashMap<char, u32> {
+    let mut letter_frequency: HashMap<char, u32> = HashMap::new();
 
     for (_index, character) in ciphertext.chars().enumerate() {
         if ALPHABET.contains(&character) {
@@ -51,8 +67,8 @@ fn calculate_letter_frequency(ciphertext:&str) -> HashMap<char, u32> {
     return letter_frequency;
 }
 
-fn get_next_most_frequent(letter_frequency:&HashMap<char, u32>) -> char {
-    let mut cipher_character:char = *(letter_frequency.keys().next()).unwrap();
+fn get_next_most_frequent(letter_frequency: &HashMap<char, u32>) -> char {
+    let mut cipher_character: char = *(letter_frequency.keys().next()).unwrap();
     let mut largest_value = *(letter_frequency.get(&cipher_character)).unwrap();
 
     for key in letter_frequency.keys() {
@@ -65,32 +81,36 @@ fn get_next_most_frequent(letter_frequency:&HashMap<char, u32>) -> char {
     return cipher_character;
 }
 
-fn replace_all_occurances(orginal_copy:&str, mut modifying_copy:Vec<char>, replacing:char, replace_with:char) -> Vec<char> { 
+fn replace_all_occurances(
+    orginal_copy: &str,
+    mut modifying_copy: Vec<char>,
+    replacing: char,
+    replace_with: char,
+) -> Vec<char> {
     for (index, character) in orginal_copy.chars().enumerate() {
         if character == replacing {
-            modifying_copy[index] = replace_with; 
+            modifying_copy[index] = replace_with;
         }
     }
 
     return modifying_copy;
 }
 
-
 #[cfg(test)]
-mod tests { 
+mod tests {
     use super::*;
 
     #[test]
     fn test_calculate_letter_frequency() {
         //given
-        let mut expected:HashMap<char, u32> = HashMap::new();
+        let mut expected: HashMap<char, u32> = HashMap::new();
         expected.insert('a', 3);
         expected.insert('b', 1);
         expected.insert('c', 1);
- 
+
         //when
         let letter_frequency = calculate_letter_frequency("abaca");
-        
+
         //then
         assert_eq!(expected, letter_frequency);
     }
@@ -98,28 +118,33 @@ mod tests {
     #[test]
     fn test_get_next_most_frequent() {
         //given
-        let mut letter_frequency:HashMap<char, u32> = HashMap::new();
+        let mut letter_frequency: HashMap<char, u32> = HashMap::new();
         letter_frequency.insert('a', 13);
         letter_frequency.insert('b', 73);
         letter_frequency.insert('c', 52);
-        
+
         let expected = 'b';
 
         //when
         let most_frequent_letter = get_next_most_frequent(&letter_frequency);
 
-        //then 
-        assert_eq!(expected, most_frequent_letter); 
+        //then
+        assert_eq!(expected, most_frequent_letter);
     }
 
     #[test]
     fn test_replace_all_occurances_single_round() {
         //given
         let orginal_copy = "abcde";
-        let expected:Vec<char> = "ebcde".chars().collect();
+        let expected: Vec<char> = "ebcde".chars().collect();
 
         //when
-        let returned = replace_all_occurances(&orginal_copy, orginal_copy.clone().chars().collect(), 'a', 'e');
+        let returned = replace_all_occurances(
+            &orginal_copy,
+            orginal_copy.clone().chars().collect(),
+            'a',
+            'e',
+        );
 
         //then
         assert_eq!(expected, returned);
@@ -129,11 +154,16 @@ mod tests {
     fn test_replace_all_occurances_multiple_rounds() {
         //given
         let orginal_copy = "abcdea";
-        let first_expected:Vec<char> = "ebcdee".chars().collect();
-        let secound_expected:Vec<char> = "ebcdae".chars().collect();
+        let first_expected: Vec<char> = "ebcdee".chars().collect();
+        let secound_expected: Vec<char> = "ebcdae".chars().collect();
 
         //when
-        let first_returned = replace_all_occurances(&orginal_copy, orginal_copy.clone().chars().collect(), 'a', 'e');
+        let first_returned = replace_all_occurances(
+            &orginal_copy,
+            orginal_copy.clone().chars().collect(),
+            'a',
+            'e',
+        );
 
         //then
         assert_eq!(first_expected, first_returned);
--- monoalphabetic-substitution-cipher/monoalphabetic-substitution-cipher/src/main.rs
+++ monoalphabetic-substitution-cipher/monoalphabetic-substitution-cipher/src/main.rs
@@ -1,10 +1,10 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::process::exit;
-use std::fs;
-use structopt::StructOpt;
 use std::collections::HashMap;
+use std::fs;
+use std::process::exit;
+use structopt::StructOpt;
 
 lazy_static! {
     static ref ALPHABET_MAP: HashMap<char, u32> = {
@@ -40,21 +40,36 @@ lazy_static! {
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "monoalphabetic-substitution-cipher", about = "A rust implementation of a monoalphabetic substitution cipher.")]
+#[structopt(
+    name = "monoalphabetic-substitution-cipher",
+    about = "A rust implementation of a monoalphabetic substitution cipher."
+)]
 struct Args {
-    #[structopt(long = "key", help = "The path to a file containing the key to use in the substitution.")]
+    #[structopt(
+        long = "key",
+        help = "The path to a file containing the key to use in the substitution."
+    )]
     key: String,
 
-    #[structopt(long = "input", help = "The path to a file containing the text to be used as input to the substitution cipher.")] 
+    #[structopt(
+        long = "input",
+        help = "The path to a file containing the text to be used as input to the substitution cipher."
+    )]
     input: String,
 
-    #[structopt(long = "decipher", help = "A flag to specify if the file content should be deciphered instead of enciphered.")]
+    #[structopt(
+        long = "decipher",
+        help = "A flag to specify if the file content should be deciphered instead of enciphered."
+    )]
     decipher: bool,
 }
 
-static ALPHABET: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+static ALPHABET: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
 
-fn main() { 
+fn main() {
     let args = Args::from_args();
 
     let file_contents = read_file(args.input);
@@ -66,44 +81,43 @@ fn main() {
         subsituted_file_contents = encipher(invert_key(key), file_contents);
     } else {
         subsituted_file_contents = encipher(key, file_contents);
-    } 
+    }
 
     print!("{}", subsituted_file_contents);
 }
 
-fn invert_key(key:Vec<char>) -> Vec<char> {
+fn invert_key(key: Vec<char>) -> Vec<char> {
     let mut inverting_key = ALPHABET.clone().to_vec();
 
     for x in 0..key.len() {
-        let i = ALPHABET_MAP.get(&key[x]).unwrap()-1;
+        let i = ALPHABET_MAP.get(&key[x]).unwrap() - 1;
         inverting_key[i as usize] = ALPHABET[x];
     }
 
     return inverting_key;
 }
 
-fn encipher(key:Vec<char>, file_contents:String) -> String { 
+fn encipher(key: Vec<char>, file_contents: String) -> String {
     let mut subsituting_file_contents: Vec<char> = file_contents.chars().collect();
 
     for (index, character) in file_contents.chars().enumerate() {
         if ALPHABET_MAP.contains_key(&character) {
-            let i = ALPHABET_MAP.get(&character).unwrap()-1;
+            let i = ALPHABET_MAP.get(&character).unwrap() - 1;
             subsituting_file_contents[index] = key[i as usize];
         }
-    }  
+    }
 
     return subsituting_file_contents.iter().collect();
 }
 
-fn read_file(filename:String) -> String {
-    let file_contents = fs::read_to_string(filename)
-        .expect("Unable to read file.");
+fn read_file(filename: String) -> String {
+    let file_contents = fs::read_to_string(filename).expect("Unable to read file.");
 
     return file_contents.to_ascii_lowercase();
 }
 
-fn validate_key(key:String) -> Vec<char> {
-    let key:String = (read_file(key)).trim().to_string(); 
+fn validate_key(key: String) -> Vec<char> {
+    let key: String = (read_file(key)).trim().to_string();
 
     if key.len() != 26 {
         println!("The key must be of length 26.");
@@ -114,32 +128,34 @@ fn validate_key(key:String) -> Vec<char> {
 }
 
 #[cfg(test)]
-mod tests { 
+mod tests {
     use super::*;
     use rstest::rstest_parametrize;
 
-    #[rstest_parametrize(expected, key,
+    #[rstest_parametrize(
+        expected, key,
         case("otduxbylwjmqifarkzphencvgs", "ofwcunytmjqhkvaslpzbdxiegr"),
         case("pqgitynxwrovjlkmfsduhzebca", "zxyswqcudmonpgkabjretlihfv"),
-        case("nkczrjsyfqtliaxhuowvebdmpg", "nvcwuizpmfblxaryjegkqtsohd"),
+        case("nkczrjsyfqtliaxhuowvebdmpg", "nvcwuizpmfblxaryjegkqtsohd")
     )]
-    fn test_invert_key(expected:&str, key:&str) {  
+    fn test_invert_key(expected: &str, key: &str) {
         //when
         let inverted_key = invert_key(key.chars().collect());
 
         //then
-        let expected_vector:Vec<char>= expected.chars().collect();
+        let expected_vector: Vec<char> = expected.chars().collect();
         assert_eq!(expected_vector, inverted_key);
     }
- 
-    #[rstest_parametrize(key, enciphering, expected, 
+
+    #[rstest_parametrize(
+        key, enciphering, expected, 
         case("gqcerpnjmhtivalosbzwxdkfyu", "defend the east wall of the castle", "erprae wjr rgzw kgii lp wjr cgzwir"),
         case("ouknpajzbhlvdrqcsxftiemwgy", "the simple substitution cipher is a cipher that has been in use for many hundreds of years", "tzp fbdcvp fiuftbtitbqr kbczpx bf o kbczpx tzot zof uppr br ifp aqx dorg zirnxpnf qa gpoxf"),
         case("ypzvihmcgqobntajdwkeruxfls", "the simple substitution cipher is quite easy to break", "eci kgnjbi krpkegeregat zgjciw gk drgei iykl ea pwiyo"),
     )]
-    fn test_encipher(key:&str, enciphering:&str, expected:&str) {  
+    fn test_encipher(key: &str, enciphering: &str, expected: &str) {
         //when
-        let returned = encipher(key.chars().collect(), enciphering.to_string()); 
+        let returned = encipher(key.chars().collect(), enciphering.to_string());
 
         //then
         assert_eq!(expected, returned);
